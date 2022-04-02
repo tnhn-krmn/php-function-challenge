a40f038151f720632327057cde4f09a426179c33
Tunahan Karaman <krmntunahan@gmail.com>
2022-04-02 12:41:01 +0300

    update

--- index.php
+++ index.php
@@ -1,8 +1,32 @@
 <?php
 
-$planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "", "", NULL];
+$planet = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "", "", NULL];
 
-$a = array_filter($planets);
+// function array_ran($planets)
+// {
+//     if (isset($planets)) 
+//     {
+//         $a = array_filter($planets);
+//     }
+// }
+// print_r(array_ran($planets));
 
- print_r($a);
 
+
+
+
+
+function array_ra($arr,$number)
+{
+    $filter_arr = array_filter($arr);
+    $arr_rand =  array_rand($filter_arr,$number);
+
+    $res = array_map(function ($item) use ($filter_arr){
+        return $filter_arr[$item];
+    },$arr_rand);
+
+    return $res;
+}
+
+
+print_r(array_ra($planet,4));
